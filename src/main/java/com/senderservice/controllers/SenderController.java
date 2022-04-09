2e84779c0d425908f7757c7878c8465970361d1a
Amitabh Saxena <45675885+amitabh19@users.noreply.github.com>
2022-04-09 19:43:43 +0530

    added methods in controller and services

--- src/main/java/com/senderservice/controllers/SenderController.java
+++ src/main/java/com/senderservice/controllers/SenderController.java
@@ -3,10 +3,36 @@ package com.senderservice.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.senderservice.entities.Transaction;
+import com.senderservice.services.SenderService;
+
 
 @RestController
 @RequestMapping("/sender")
 public class SenderController {
 
+	@Autowired
+	private SenderService senderService;
+
+	@PostMapping(path = "/sendTransaction")
+	public ResponseEntity<?> encryptTransactionObject(@RequestBody Transaction transaction) {
+		try {
+			return senderService.sendTrans(transaction);
+		} catch (Exception e) {
+			if (e.getLocalizedMessage().isEmpty()) {
+				return new ResponseEntity<String>("wrong transaction object sent",
+						new ResponseEntity<>(HttpStatus.BAD_REQUEST).getStatusCode());
+			} else {
+				return new ResponseEntity<String>(e.getLocalizedMessage(),
+						new ResponseEntity<>(HttpStatus.BAD_REQUEST).getStatusCode());
+			}
+		}
+	}
 	
 }
--- src/main/java/com/senderservice/services/SenderService.java
+++ src/main/java/com/senderservice/services/SenderService.java
@@ -3,7 +3,16 @@
  */
 package com.senderservice.services;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.senderservice.entities.Transaction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+import com.senderservice.encrypt.EncryptObject;
+import com.senderservice.repositories.TransactionRepositories;
 
 
 /**
@@ -12,5 +21,34 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SenderService {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private TransactionRepositories tr;
+
+	/*
+	 * public SenderService(RestTemplate restTemplate) { this.restTemplate =
+	 * restTemplate; }
+	 */
+
+	public ResponseEntity<Transaction> sendTrans(Transaction t) throws NullPointerException {
+
+		if (t.getAccountNumber()==null || t.getAccountNumber().isEmpty() || t.getAccountFrom().isEmpty() || t.getAmount().isEmpty()
+				|| t.getCurrency().isEmpty() || t.getType().isEmpty()) {
+			throw new NullPointerException("no field should be empty");
+		}
+
+		t.setAccountNumber(EncryptObject.encrypt(t.getAccountNumber()));
+		t.setAccountFrom(EncryptObject.encrypt(t.getAccountFrom()));
+		t.setAmount(EncryptObject.encrypt(t.getAmount()));
+		t.setCurrency(EncryptObject.encrypt(t.getCurrency()));
+		t.setType(EncryptObject.encrypt(t.getType()));
+		t = tr.save(t);
+		System.out.println("Sender Obj" + t.toString());
+		String receiverURL = "http://localhost:9002/receiver/receiveTransaction";
+		return restTemplate.postForEntity(receiverURL, t, Transaction.class);
+	}
+
 
 }
